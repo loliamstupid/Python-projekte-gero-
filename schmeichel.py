e5c1e7638021d167428888d27ae3a7a9139bfd4a
loliamstupid <giro.batt@gmail.com>
2025-01-19 01:46:37 +0100

    NEW Version WUHUU more answers and better response sysem that actually works with sentences.

---
+++ schmeichel.py
@@ -0,0 +1,9 @@
+
+import random
+
+adjektiv = ['beste ', 'schönste', 'größte']
+nomen = ['mensch ', 'hecht ','freund', 'kumpel ', 'pogrammierer' ]
+
+print('du bist der ')
+print(random.choice(adjektiv))
+print(random.choice(nomen))
