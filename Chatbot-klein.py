45f77525d49ba43d4a2bdd8379e67ab6b33d9eda
loliamstupid <giro.batt@gmail.com>
2025-01-19 01:43:27 +0100

    NEW Version WUHUU more answers and better response sysem that actually works with sentences.

--- Chatbot-klein.py
+++ Chatbot-klein.py
@@ -50,7 +50,7 @@ reaktionsantworten = {"hallo":   "aber Hallo",
                       'was ist dein lieblingsbuch':    'ich bin ein chatbot ich habe keinen lieblingsbuch',
                       'was ist dein lieblingsfilm':    'ich bin ein chatbot ich habe keinen lieblingsfilm',
                       'was ist dein lieblingshobby':    'ich bin ein chatbot ich habe keinen lieblingshobby',
-                      'help':   'kontakt unter Github'
+
 
 
 
